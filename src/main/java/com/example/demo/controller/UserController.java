3f172c6375fbe30cadf93656ccb10a684d406086
Pavi99 <pavithra@smbapps.com.au>
2022-12-24 13:59:51 +0530

    save user committed

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,19 +1,24 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.UserDTO;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/v/user")
 @CrossOrigin
 public class UserController {
+    @Autowired
+    private UserService userService;
     @GetMapping("/getUser")
     public String getUser(){
         return "Simple-root";
     }
 
     @PostMapping("/saveUser")
-    public String saveUser(){
-        return "user Saved!";
+    public UserDTO saveUser(@RequestBody UserDTO userDTO) {
+        return userService.saveUser(userDTO);
     }
 
     @PutMapping("/putUser")
