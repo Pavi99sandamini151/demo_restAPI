c5313cecf53b89f10e7b8758bf2627bc37dbb0cd
Pavi99 <pavithra@smbapps.com.au>
2022-12-24 14:08:51 +0530

    get user committed

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,6 +5,8 @@ import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "api/v/user")
 @CrossOrigin
@@ -12,8 +14,8 @@ public class UserController {
     @Autowired
     private UserService userService;
     @GetMapping("/getUser")
-    public String getUser(){
-        return "Simple-root";
+    public List<UserDTO> getUser(){
+        return userService.getAllUsers();
     }
 
     @PostMapping("/saveUser")
